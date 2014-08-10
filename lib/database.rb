b10ab093bf1f0a2ada70e748aa0c5ccc1fd6c98b
Logan Howard <loganh@pobox.com>
2014-08-10 14:10:59 -0700

    add aws stuff

--- lib/database.rb
+++ lib/database.rb
@@ -2,8 +2,8 @@ require 'sequel'
 require 'konfiguration'
 require 'rubyflake'
 
-if ENV['OPENSHIFT_POSTGRESQL_DB_URL']
-  DB = Sequel.connect(ENV['OPENSHIFT_POSTGRESQL_DB_URL'])
+if ENV['RACK_ENV'] == 'production'
+  DB = Sequel.connect('postgres://logan:Dj3AsZqAxG%h9?@aag9w68z5p862l.cehf3tpvjpkp.us-west-1.rds.amazonaws.com:5432')
 else
   DB = Sequel.connect(Konfiguration.database(:uri))
 end
