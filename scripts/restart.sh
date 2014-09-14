b8b6b5499ede73f21e7c089d5743171c82b6dfbd
Logan Howard <logan@oflogan.com>
2014-09-14 14:14:02 -0700

    add restart scripts

---
+++ scripts/restart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# this is meant to be run from within the root git directory
+
+# grep processes with `thin` in them, list their PIDs, kill them softly
+ps ax | fgrep thin | awk '{print $1}' | tr '\n' ' ' | xargs kill -15
+
+# just in case... (kill -2)
+sleep 1
+ps ax | fgrep thin | awk '{print $1}' | tr '\n' ' ' | xargs kill -2
+
+# just in case... (kill -9)
+sleep 1
+ps ax | fgrep thin | awk '{print $1}' | tr '\n' ' ' | xargs kill -9
+
+thin -R config.ru -p 7000 -s 3 start
+
+echo 'Done restarting.'
