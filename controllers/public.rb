f9c0201e9cc2684ab4308fdf89d7b9739c1747d1
Logan Howard <logan@oflogan.com>
2014-10-11 12:25:11 -0700

    hopefully fix cache not working

--- controllers/public.rb
+++ controllers/public.rb
@@ -1,45 +1,47 @@
 require 'curl'
 require 'lib/cache'
 
-module Firebots::InternalAPI::Controllers
+module Firebots
+  module InternalAPI::Controllers
 
-  class Public < Kenji::Controller
+    class Public < Kenji::Controller
 
-    get '/about' do
-      url = 'https://gist.githubusercontent.com/PikaDotus/f65b98b347cc12816c36/raw/about.md'
-      unless message = Cache.get('about')
-        Cache.set('about', message = Curl.get(url), 60)
+      get '/about' do
+        url = 'https://gist.githubusercontent.com/PikaDotus/f65b98b347cc12816c36/raw/about.md'
+        unless message = Cache.get('about')
+          Cache.set('about', message = Curl.get(url), 60)
+        end
+
+        {
+          status: 200,
+          message: message.body_str.force_encoding('UTF-8'),
+        }
       end
 
-      {
-        status: 200,
-        message: message.body_str.force_encoding('UTF-8'),
-      }
-    end
+      get '/important-info' do
+        url = 'https://gist.githubusercontent.com/PikaDotus/7a9e39020c0276c3034a/raw/important-info.md'
+        unless message = Cache.get('important-info')
+          Cache.set('important-info', message = Curl.get(url), 60)
+        end
 
-    get '/important-info' do
-      url = 'https://gist.githubusercontent.com/PikaDotus/7a9e39020c0276c3034a/raw/important-info.md'
-      unless message = Cache.get('important-info')
-        Cache.set('important-info', message = Curl.get(url), 60)
+        {
+          status: 200,
+          message: message.body_str.force_encoding('UTF-8'),
+        }
       end
 
-      {
-        status: 200,
-        message: message.body_str.force_encoding('UTF-8'),
-      }
-    end
+      get '/message' do
+        url = 'https://gist.githubusercontent.com/PikaDotus/97bb3fdebbc0446c964a/raw/alert'
+        unless message = Cache.get('message')
+          Cache.set('message', message = Curl.get(url), 60)
+        end
 
-    get '/message' do
-      url = 'https://gist.githubusercontent.com/PikaDotus/97bb3fdebbc0446c964a/raw/alert'
-      unless message = Cache.get('message')
-        Cache.set('message', message = Curl.get(url), 60)
+        {
+          status: 200,
+          message: message.body_str.force_encoding('UTF-8')
+        }
       end
 
-      {
-        status: 200,
-        message: message.body_str.force_encoding('UTF-8')
-      }
     end
-
   end
 end
