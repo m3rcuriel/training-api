80aa5f156e007ff2c7be8fab1d4c0138dc7ce563
Logan Howard <logan@oflogan.com>
2014-10-11 12:22:28 -0700

    add caching to public stuff

--- controllers/public.rb
+++ controllers/public.rb
@@ -1,36 +1,43 @@
 require 'curl'
+require 'lib/cache'
 
 module Firebots::InternalAPI::Controllers
 
   class Public < Kenji::Controller
 
     get '/about' do
-      http = Curl.get('https://gist.githubusercontent.com/PikaDotus/'\
-        'f65b98b347cc12816c36/raw/about.md')
+      url = 'https://gist.githubusercontent.com/PikaDotus/f65b98b347cc12816c36/raw/about.md'
+      unless message = Cache.get('about')
+        Cache.set('about', message = Curl.get(url), 60)
+      end
 
       {
         status: 200,
-        message: http.body_str.force_encoding('UTF-8'),
+        message: message.body_str.force_encoding('UTF-8'),
       }
     end
 
     get '/important-info' do
-      http = Curl.get('https://gist.githubusercontent.com/PikaDotus/'\
-        '7a9e39020c0276c3034a/raw/important-info.md')
+      url = 'https://gist.githubusercontent.com/PikaDotus/7a9e39020c0276c3034a/raw/important-info.md'
+      unless message = Cache.get('important-info')
+        Cache.set('important-info', message = Curl.get(url), 60)
+      end
 
       {
         status: 200,
-        message: http.body_str.force_encoding('UTF-8'),
+        message: message.body_str.force_encoding('UTF-8'),
       }
     end
 
     get '/message' do
-      http = Curl.get('https://gist.githubusercontent.com/PikaDotus/'\
-        '97bb3fdebbc0446c964a/raw/alert')
+      url = 'https://gist.githubusercontent.com/PikaDotus/97bb3fdebbc0446c964a/raw/alert'
+      unless message = Cache.get('message')
+        Cache.set('message', message = Curl.get(url), 60)
+      end
 
       {
         status: 200,
-        message: http.body_str.force_encoding('UTF-8')
+        message: message.body_str.force_encoding('UTF-8')
       }
     end
 
