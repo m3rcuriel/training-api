a9407311874779cbdfa841bde754e8fcdff4796b
Logan Howard <logan@oflogan.com>
2015-01-18 14:00:23 -0800

    update gist links

--- controllers/public.rb
+++ controllers/public.rb
@@ -7,7 +7,7 @@ module Firebots
     class Public < Kenji::Controller
 
       get '/about' do
-        url = 'https://gist.githubusercontent.com/PikaDotus/f65b98b347cc12816c36/raw/about.md'
+        url = 'https://gist.githubusercontent.com/glinia/f65b98b347cc12816c36/raw/about.md'
         unless message = Cache.get('about')
           Cache.set('about', message = Curl.get(url).body_str, 60)
         end
@@ -19,7 +19,7 @@ module Firebots
       end
 
       get '/important-info' do
-        url = 'https://gist.githubusercontent.com/PikaDotus/7a9e39020c0276c3034a/raw/important-info.md'
+        url = 'https://gist.githubusercontent.com/glinia/7a9e39020c0276c3034a/raw/important-info.md'
         unless message = Cache.get('important-info')
           Cache.set('important-info', message = Curl.get(url).body_str, 60)
         end
@@ -31,7 +31,7 @@ module Firebots
       end
 
       get '/message' do
-        url = 'https://gist.githubusercontent.com/PikaDotus/97bb3fdebbc0446c964a/raw/alert'
+        url = 'https://gist.githubusercontent.com/glinia/97bb3fdebbc0446c964a/raw/alert'
         unless message = Cache.get('message')
           Cache.set('message', message = Curl.get(url).body_str, 60)
         end
