b71021746288d494a471cd61524c546a6da356a5
Lee Mracek <lee.mracek@gmail.com>
2015-09-06 20:08:54 -0700

    Change gists to mine (not Logans)

--- controllers/public.rb
+++ controllers/public.rb
@@ -7,7 +7,7 @@ module Firebots
     class Public < Kenji::Controller
 
       get '/about' do
-        url = 'https://gist.githubusercontent.com/glinia/f65b98b347cc12816c36/raw/about.md'
+        url = 'https://gist.githubusercontent.com/m3rcuriel/a617b50b0296b792c180/raw/about.md'
         unless message = Cache.get('about')
           Cache.set('about', message = Curl.get(url).body_str, 60)
         end
@@ -19,7 +19,7 @@ module Firebots
       end
 
       get '/important-info' do
-        url = 'https://gist.githubusercontent.com/glinia/7a9e39020c0276c3034a/raw/important-info.md'
+        url = 'https://gist.githubusercontent.com/m3rcuriel/c7b1088531115c7b53d0/raw/important-info.md'
         unless message = Cache.get('important-info')
           Cache.set('important-info', message = Curl.get(url).body_str, 60)
         end
